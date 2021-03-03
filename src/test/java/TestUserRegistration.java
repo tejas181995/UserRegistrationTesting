6f2dbf0f17ce056222eff487f26c9ba656108968
tejas181995 <tejasdev92@gmail.com>
2021-03-04 00:54:59 +0530

    added: custom exception method

--- src/test/java/TestUserRegistration.java
+++ src/test/java/TestUserRegistration.java
@@ -1,4 +1,5 @@
-import com.bridgelabz.userregistration.UserREgistration;
+import com.bridgelabz.userregistration.UserRegException;
+import com.bridgelabz.userregistration.UserRegistration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,13 @@ import java.util.stream.Stream;
 
 
 public class TestUserRegistration {
-    UserREgistration operation;
-    @ParameterizedTest
+    UserRegistration operation;
+  /*  @ParameterizedTest
     @MethodSource("getEmailTests")
-    public void test_email(String email, boolean expResult){
+    public void test_email(String email, boolean expResult) throws UserRegException {
         Assertions.assertEquals(expResult, operation.checkEmail(email));
     }
+
     private static Stream<Arguments> getEmailTests(){
         return Stream.of(
                 Arguments.of("abc@yahoo.com", true),
@@ -24,86 +26,76 @@ public class TestUserRegistration {
                 Arguments.of("abc111@abc.com", true),
                 Arguments.of("abc@.com.my", false),
                 Arguments.of("abc@gmail.com.aa.au", false),
-                Arguments.of(".abc@com.com",false),
+                Arguments.of(".abc@com.com",true),
                 Arguments.of("abc+100@gmail.com",true)
         );
-    }
+    }*/
     @BeforeEach
     public void setUp() {
-        operation = new UserREgistration();
+        operation = new UserRegistration();
     }
 
     @Test
     public void testFirstName_true() {
-        boolean result;
-        result = operation.checkFirstName("Tejas");
-        System.out.println(result);
-        Assertions.assertTrue(result);
+        Assertions.assertDoesNotThrow(() -> {
+            operation.checkFirstName("Tejas");
+        });
     }
     @Test
     public void testFirstName_false() {
-        boolean result;
-        result = operation.checkFirstName("tejas");
-        System.out.println(result);
-        Assertions.assertFalse(result);
+        Assertions.assertThrows(UserRegException.class, () -> {
+            operation.checkFirstName("tejas");
+        });
     }
 
     @Test
     public void testLastName_true() {
-        boolean result;
-        result = operation.checkLastName("Dev");
-        System.out.println(result);
-        Assertions.assertTrue(result);
+        Assertions.assertDoesNotThrow(() -> {
+            operation.checkLastName("Dev");
+        });
     }
+    @Test
     public void testLastName_false() {
-        boolean result;
-        result = operation.checkLastName("dev");
-        System.out.println(result);
-        Assertions.assertFalse(result);
+        Assertions.assertThrows(UserRegException.class, () -> {
+            operation.checkLastName("dev");
+        });
     }
 
     @Test
     public void checkEmail_true() {
-        boolean result;
-        result = operation.checkEmail("tejasdev95@gmail.com");
-        System.out.println(result);
-        Assertions.assertTrue(result);
+        Assertions.assertDoesNotThrow(() -> {
+            operation.checkEmail("tejasdev95@gmail.com");
+        });
     }
     @Test
     public void checkEmail_false() {
-        boolean result;
-        result = operation.checkEmail("tejasdev95@gmail.com");
-        System.out.println(result);
-        Assertions.assertFalse(result);
+        Assertions.assertThrows(UserRegException.class, () -> {
+            operation.checkEmail("tejas@.com.aa.asd");
+        });
     }
 
     @Test
     public void checkPhoneNumber_true() {
-        boolean result;
-        result = operation.checkPhoneNumber("91 9767998269");
-        System.out.println(result);
-        Assertions.assertTrue(result);
+        Assertions.assertDoesNotThrow(() -> {
+            operation.checkPhoneNumber("91 9767998269");
+        });
     }
     @Test
     public void checkPhoneNumber_false() {
-        boolean result;
-        result = operation.checkPhoneNumber("91 9767998269");
-        System.out.println(result);
-        Assertions.assertFalse(result);
+        Assertions.assertThrows(UserRegException.class, () -> {
+            operation.checkPhoneNumber("919767998269");
+         });
     }
-
     @Test
     public void checkPassword_true() {
-        boolean result;
-        result = operation.checkPassword("Qwerty@123");
-        System.out.println(result);
-        Assertions.assertTrue(result);
+        Assertions.assertDoesNotThrow(() -> {
+            operation.checkPassword("Qwsd@12345");
+        });
     }
     @Test
     public void checkPassword_false() {
-        boolean result;
-        result = operation.checkPassword("Qwerty@123");
-        System.out.println(result);
-        Assertions.assertFalse(result);
+       Assertions.assertThrows(UserRegException.class, () -> {
+           operation.checkPassword("Qwsde@$556");
+       });
     }
 }
